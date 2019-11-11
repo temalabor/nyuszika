f97a949903169ba78bdebcc26a454b62c38323d6
Farkas Dániel <farkas1.daniel@gmail.com>
2019-11-11 14:26:31 +0100

    LVL gen demo

---
+++ Assets/Scripts/Level Generation/freemove.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class freemove : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    public int rndszorzo = 1;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.A))
+            rb.transform.position = rb.transform.position + rndszorzo*Time.deltaTime*Vector3.left;
+        if (Input.GetKey(KeyCode.W))
+            rb.transform.position = rb.transform.position + rndszorzo*Time.deltaTime*Vector3.up;
+        if (Input.GetKey(KeyCode.D))
+            rb.transform.position = rb.transform.position + rndszorzo*Time.deltaTime*Vector3.right;
+        if (Input.GetKey(KeyCode.S))
+            rb.transform.position = rb.transform.position + rndszorzo*Time.deltaTime*Vector3.down;
+    }
+}
